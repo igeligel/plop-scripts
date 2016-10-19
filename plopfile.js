b6fa3ffd54dd9a588144196e74360ca86be0ad4d
igeligel <kevinigeligeligel@gmail.com>
2016-10-19 20:31:41 +0200

    feat(template)
    add first template for generating general java ee services which is
    using the entity manager by dependency injection

---
+++ plopfile.js
@@ -0,0 +1,29 @@
+module.exports = (plop) => {
+  // Here we'll define our generators
+  // We declare a new generator called "module"
+  plop.setGenerator("module", {
+
+    // Succintly describes what generator does.
+    description: "Create a new service",
+
+    // Get inputs from the user.
+    // That's Inquirer.js doing the job behind the hood.
+    prompts: [{
+      type: "input",
+      name: "className",
+      message: "What is the entity class name of your service?"
+    }, {
+      type: "input",
+      name: "packageName",
+      message: "What is the package name of your service?"
+    }],
+
+    // List of actions to take.
+    // Here we "add" new files from our templates.
+    actions: [{
+      type: "add",
+      path: "./{{className}}Service.java",
+      templateFile: "templates/javaee/Service.java"
+    }]
+  });
+};
---
+++ templates/javaee/Service.java
@@ -0,0 +1,36 @@
+package {{packageName}};
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class {{className}}Service {
+  @PersistenceContext
+  protected EntityManager entityManager;
+
+  public {{className}}Service() {
+  }
+
+  public void persist({{className}} {{camelCase className}}) {
+    entityManager.persist({{camelCase className}});
+  }
+
+  public {{className}} findById(Integer id) {
+    return entityManager.find({{className}}.class, id);
+  }
+
+  @SuppressWarnings("unchecked")
+  public List<{{className}}> findAll() {
+    String sqlQuery = "SELECT e FROM {{className}} e";
+    List<{{className}}> allEntities = entityManager.createQuery(sqlQuery).getResultList();
+    return allEntities;
+  }
+
+  public {{className}} delete(int id) {
+    {{className}} entityToBeRemoved = findById(id);
+    entityManager.remove(entityToBeRemoved);
+    return entityToBeRemoved;
+  }
+}
