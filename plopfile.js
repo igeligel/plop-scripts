2a6688dacbc164ea05ced1dc647db52592f309ce
igeligel <kevinigeligeligel@gmail.com>
2016-10-20 20:36:36 +0200

    feat(plop)
    add new plop case for server interfaces

--- plopfile.js
+++ plopfile.js
@@ -58,4 +58,34 @@ module.exports = (plop) => {
       templateFile: "templates/javaee/Resource.java"
     }]
   });
+
+  plop.setGenerator("ServiceInterface", {
+
+    // Succintly describes what generator does.
+    description: "Create a new service interface",
+
+    // Get inputs from the user.
+    // That's Inquirer.js doing the job behind the hood.
+    prompts: [{
+      type: "input",
+      name: "className",
+      message: "What is the entity class name of your service interface?"
+    }, {
+      type: "input",
+      name: "packageName",
+      message: "What is the package name of your service?"
+    }, {
+      type: "input",
+      name: "filePath",
+      message: "Where should the final file be at?"
+    }],
+
+    // List of actions to take.
+    // Here we "add" new files from our templates.
+    actions: [{
+      type: "add",
+      path: "{{filePath}}" + "/{{className}}ServiceInterface.java",
+      templateFile: "templates/javaee/ServiceInterface.java"
+    }]
+  });
 };
---
+++ templates/javaee/ServiceInterface.java
@@ -0,0 +1,34 @@
+package de.ostfalia.groupfour.integration.interfaces;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/{{camelCase className}}")
+public interface {{className}}ServiceInterface {
+  @GET
+  @Path("/{{ resourceid className }}")
+  @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+  Response get{{className}}ById(@PathParam("{{camelCase className}}Id") Integer cityId);
+	
+  @POST
+  @Path("/")
+  @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+  Response post{{className}}({{className}}PersistModel {{camelCase className}}PostModel);
+	
+  @PUT
+  @Path("/")
+  @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+  Response put{{className}}({{className}}PersistModel {{camelCase className}}PostModel);
+	
+  @DELETE
+  @Path("/{{ resourceid className }}")
+  Response delete{{className}}(@PathParam("{{camelCase className}}Id") Integer {{camelCase className}}Id);
+}
